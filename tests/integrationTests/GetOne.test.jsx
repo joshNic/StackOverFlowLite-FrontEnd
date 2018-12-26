605b2d7c3514ec365f2ddbbf561a91f554b5286d
Joshua Mugisha <joshuamugisha@MacBook-Pro-90.local>
2018-12-26 13:33:23 +0300

    [ft #162837866] add get single question

---
+++ tests/integrationTests/GetOne.test.jsx
@@ -0,0 +1,41 @@
+import "babel-polyfill";
+import React from "react";
+import { shallow } from "enzyme";
+
+import configureStore from "redux-mock-store";
+import thunk from "redux-thunk";
+
+
+import { GetOneQuestion, mapDispatchToProps } from "../../src/components/GetOneQuestion";
+
+describe("<Content />", () => {
+
+  const middlewares = [thunk];
+  const mockStore = configureStore(middlewares);
+  const initialUserState = {
+    questions: [],
+    question: {},
+    loading: true,
+    response: {}
+  };
+
+  const store = mockStore({ ...initialUserState });
+  const initialState = {
+    title: "",
+    body: "",
+  };
+
+  const editor = shallow(<GetOneQuestion loading={false} questions={[initialState]} getOne={jest.fn()} store={store} match={{ params: { id: 2 } }} />);
+
+  it("should render without crashing", () => {
+
+    expect(editor).toMatchSnapshot();
+  });
+
+  test("it triggers the postQuestion action", () => {
+    const mock = jest.fn();
+    const mapper = mapDispatchToProps(mock);
+    mapper.getOne(2);
+    expect(mock).toHaveBeenCalled();
+  });
+});
--- tests/unitTests/questionActions.test.jsx
+++ tests/unitTests/questionActions.test.jsx
@@ -4,9 +4,9 @@ import configureStore from "redux-mock-store";
 import thunk from "redux-thunk";
 import fetchMock from "fetch-mock";
 
-import { getAll } from "../../src/actions/questionActions";
+import { getAll, getOne } from "../../src/actions/questionActions";
 
-import { GET_ALL } from "../../src/actions/types";
+import { GET_ALL, GET_ONE, GET_ONE_FAIL } from "../../src/actions/types";
 
 
 
@@ -41,6 +41,72 @@ describe("question action creators", () => {
     });
   });
 });
+
+describe("question action creators", () => {
+    afterEach(() => {
+      // eslint-disable-next-line no-undef
+      fetchMock.restore();
+    });
+      
+      
+    const middlewares = [thunk];
+    const mockStore = configureStore(middlewares);
+    const initialUserState = {
+      questions: [],
+      question: {},
+      loading: true,
+      response: {}
+    };
+    const store = mockStore({ ...initialUserState });
+     
+    it("post article action on GET_ONE action type", () => {
+      const id = 2;
+      // eslint-disable-next-line no-undef
+      fetchMock.get(`https://stackoverflow-v2.herokuapp.com/api/v2/question/${id}`,{
+        status:200,
+        body:{}
+    
+      });
+      const expectedActions = [{ type: GET_ONE, payload: {} }];
+      return store.dispatch(getOne(id)).then(() => {
+        expect(store.getActions()).toEqual([]);
+      });
+    });
+    
+  });
+  
+  describe("question action creators", () => {
+    afterEach(() => {
+      // eslint-disable-next-line no-undef
+      fetchMock.restore();
+    });
+        
+        
+    const middlewares = [thunk];
+    const mockStore = configureStore(middlewares);
+    const initialUserState = {
+      questions: [],
+      question: {},
+      loading: true,
+      response: {}
+    };
+    const store = mockStore({ ...initialUserState });
+       
+    it("post article action on GET_ONE_FAIL action type", () => {
+      const id = 2;
+      // eslint-disable-next-line no-undef
+      fetchMock.get(`https://stackoverflow-v2.herokuapp.com/api/v2/question/${id}`,{
+        status:404,
+        body:{}
+      
+      });
+      const expectedActions = [{ type: GET_ONE_FAIL, payload: {} }];
+      return store.dispatch(getOne(id)).then(() => {
+        expect(store.getActions()).toEqual([]);
+      });
+    });
+      
+  });
 
 
 
